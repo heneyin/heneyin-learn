f4af37efff04d6f2a1a43516e576392a5785855a
heneyin <1103693087@qq.com>
2022-04-16 13:42:24 +0800

    feat(gradle): 添加 gradle 核心学习

---
+++ learn-gradle/project-java/src/test/java/com/heneyin/example/MainTest.java
@@ -0,0 +1,16 @@
+package com.heneyin.example;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author hongliang.yin/Heneyin
+ * @date 2022/4/1
+ */
+public class MainTest {
+
+    @Test
+    public void test() {
+        System.out.println("test");
+    }
+
+}
